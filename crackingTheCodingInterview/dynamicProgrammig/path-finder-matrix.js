00a6866021530ab72af504d19c0e01549c52297f
Zeljko <zeljko.cavic@playstudios.com>
2022-08-03 09:59:41 +0200

    BFS

--- crackingTheCodingInterview/dynamicProgrammig/path-finder-matrix.js
+++ crackingTheCodingInterview/dynamicProgrammig/path-finder-matrix.js
@@ -29,24 +29,60 @@ const findPath = (n, m, offLimits) => {
     return makeStep(1, 1);
 };
 
-const temp = findPath(10, 10, [
-    "1,2",
-    "2,2",
-    "4,2",
-    "5,2",
-    "6,2",
-    "7,2",
-    "8,2",
-    "9,2",
-    "10,2",
-    "1,4",
-    "2,4",
-    "3,4",
-    "4,4",
-    "5,4",
-    "6,4",
-    "8,4",
-    "9,4",
-    "10,4"
-]);
+const temp = findPath(5, 5, ["1,2", "3,2", "4,2", "5,2", "4,4", "4,5"]);
 console.log(temp);
+
+const findPath_bfs = (startX, startY, map) => {
+    // initialization
+    const queueX = [];
+    const queueY = [];
+    const lengthX = map[0].length;
+    const lengthY = map.length;
+
+    // add start point
+    queueX.push(startX);
+    queueY.push(startY);
+
+    // start BFS
+    while (queueX.length > 0 && queueY.length > 0) {
+        const x = queueX.pop();
+        const y = queueY.pop();
+
+        // check is End
+        if (map[x][y] === "E") {
+            return "success";
+        }
+
+        // visit
+        map[x][y] = "x";
+
+        // add next layer
+        if (x + 1 < lengthX && map[x + 1][y] !== "x" && map[x + 1][y] !== "#") {
+            queueX.push(x + 1);
+            queueY.push(y);
+        }
+        if (y + 1 < lengthY && map[x][y + 1] !== "x" && map[x][y + 1] !== "#") {
+            queueX.push(x);
+            queueY.push(y + 1);
+        }
+        if (x - 1 > 0 && map[x - 1][y] !== "x" && map[x - 1][y] !== "#") {
+            queueX.push(x - 1);
+            queueY.push(y);
+        }
+        if (y - 1 > 0 && map[x][y - 1] !== "x" && map[x][y - 1] !== "#") {
+            queueX.push(x);
+            queueY.push(y - 1);
+        }
+    }
+    return false;
+};
+const map = [
+    [" ", "#", " ", " ", " "],
+    [" ", " ", " ", " ", " "],
+    [" ", "#", " ", " ", " "],
+    [" ", "#", " ", "#", "#"],
+    [" ", "#", " ", " ", "E"]
+];
+
+const temp1 = findPath_bfs(0, 0, map);
+console.log(temp1);
